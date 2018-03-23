1c9acdaf2c70b9d2be5ce8817f0317bf577f222f
Hugh Aristotle Powell <hugh@hughpowell.co.uk>
2018-03-23 15:31:44 +0000

    Point the test runner to the cucumber features

--- src/test/java/uk/co/hughpowell/payments/CucumberTest.java
+++ src/test/java/uk/co/hughpowell/payments/CucumberTest.java
@@ -1,7 +1,10 @@
 package uk.co.hughpowell.payments;
 
 import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/uk/co/hughpowell/features")
 public class CucumberTest { }
